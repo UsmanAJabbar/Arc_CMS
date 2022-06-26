e0d73d27d25e6d73b7e154076caf2cfa2de11182
Usman Abdul Jabbar <ujabbar@idonate.com>
2022-06-25 22:51:37 -0500

    Added documentation to GraphQL endpoints

--- arc/graphql/query.py
+++ arc/graphql/query.py
@@ -9,7 +9,11 @@ from arc.post.graphql.schema import Post
 class Query:
     @strawberry.field
     @db_session
-    def post_by_id(self, id: UUID) -> Post:
+    def post(self, id: UUID) -> Post:
+        """Takes in an ID and returns the post associated with it
+
+        Raises: ValueError if ID is not found/invalid
+        """
         from arc.post.models import Post as PostModel
         from arc.user.models import User as UserModel
         from arc.utils import row_to_dict
@@ -26,24 +30,8 @@ class Query:
 
     @strawberry.field
     @db_session
-    def post_by_slug(self, slug: str) -> Post:
-        from arc.post.models import Post as PostModel
-        from arc.user.models import User as UserModel
-        from arc.utils import row_to_dict
-
-        try:
-            post = select(
-                p for p in PostModel
-                if p.url == slug
-            ).prefetch(UserModel)[:][0]
-        except IndexError:
-            raise ValueError('Invalid UUID')
-
-        return Post(**row_to_dict(post))
-
-    @strawberry.field
-    @db_session
-    def n_posts(self, limit: int = 3, order: str = 'desc') -> List[Post]:
+    def posts(self, limit: int = 3, order: str = 'desc') -> List[Post]:
+        """Takes in a @limit and @order parameter and returns a list of posts"""
         from arc.post.models import Post as PostModel
         from arc.user.models import User as UserModel
         from arc.utils import row_to_dict
@@ -65,6 +53,27 @@ class Query:
             Post(**row_to_dict(r))
             for r in posts
         ]
+
+    @strawberry.field
+    @db_session
+    def post_by_slug(self, slug: str) -> Post:
+        """Takes in a slug as a string and returns the post associated with it
+
+        Raises: ValueError if ID is not found/invalid
+        """
+        from arc.post.models import Post as PostModel
+        from arc.user.models import User as UserModel
+        from arc.utils import row_to_dict
+
+        try:
+            post = select(
+                p for p in PostModel
+                if p.url == slug
+            ).prefetch(UserModel)[:][0]
+        except IndexError:
+            raise ValueError('Invalid UUID')
+
+        return Post(**row_to_dict(post))
 
 
 schema = strawberry.Schema(Query)
